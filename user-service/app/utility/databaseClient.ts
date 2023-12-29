5ed4c8f272ce68dd00a23ca1b2149f255d257266
buckcode16 <buckcode16@gmail.com>
2023-12-29 22:06:41 +0800

    Complete E14

--- user-service/app/utility/databaseClient.ts
+++ user-service/app/utility/databaseClient.ts
@@ -2,10 +2,24 @@ import { Client } from 'pg'
 
 export const DBClient = () => {
   return new Client({
-    host: 'user-service.cv402emcgt18.ap-southeast-1.rds.amazonaws.com',
-    user: 'postgres',
+    host: 'ec2-52-221-214-109.ap-southeast-1.compute.amazonaws.com',
+    user: 'user_service',
     database: 'user_service',
-    password: 'postgres',
+    password: 'user_service',
     port: 5432,
   })
 }
+
+// RDS Managed Instance
+// host: 'user-service.cv402emcgt18.ap-southeast-1.rds.amazonaws.com',
+// user: 'postgres',
+// database: 'user_service',
+// password: 'postgres',
+// port: 5432,
+
+// EC2 Self Managed RDS Instance
+// host: 'ec2-52-221-214-109.ap-southeast-1.compute.amazonaws.com',
+// user: 'user_service',
+// database: 'user_service',
+// password: 'user_service',
+// port: 5432,
